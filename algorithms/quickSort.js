db2de23dec871bdc900a5d7dec2730e67f37d2e9
ellier7 <eroussopoulos@gmail.com>
2016-11-17 21:21:02 -0500

    Quick sort complete; test pass

---
+++ algorithms/quickSort.js
@@ -0,0 +1,22 @@
+// used often
+'use strict';
+
+const quickSort = array => {
+	if(array.length <= 1){
+		return array
+	}
+	const pivot = array[array.length - 1]
+	const left = []
+	const right = []
+	for(let i = 0; i < array.length - 1; i++){
+		if(array[i] < pivot){
+			left.push(array[i])
+		}else{
+			right.push(array[i])
+		}
+	}
+	return quickSort(left).concat(pivot, quickSort(right));
+}
+
+module.exports = { quickSort }
+// time complexity: O(nlogn)
